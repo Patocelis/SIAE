6f44433e6b8afef6006ce381b8c858db59169f96
benjax192 <bmoralesp@usm.cl>
2025-06-20 20:40:39 -0400

    Creacion de separacion por roles

--- usuarios.cpp
+++ usuarios.cpp
@@ -5,16 +5,18 @@
 #include "usuarios.h"
 
 #include <iostream>
-usuario::usuario(bool Profe, long Rut, int Contra):profesor(Profe), rut(Rut), contrasena(Contra) {}
+usuario::usuario(bool Profe, long Rut, std::string Contra):profesor(Profe), rut(Rut), contrasena(Contra) {}
 
 void usuario::profecontra() {
     std::string ingresarcontra;
     std::cout << "Ingrese contrasena para acceder como profesor" << std::endl; std::cin>>ingresarcontra;
     if (contrasena == ingresarcontra) {
         profesor = true;
+        rol = "Profesor";
     }
     else {
         profesor = false;
+        rol = "Alumno";
     }
 }
 
